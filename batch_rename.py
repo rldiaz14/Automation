2559d0228dadd5053bfb9d63c6b92508248cb143
rldiaz14 <rldiaz14.1@gmail.com>
2023-02-01 18:01:28 -0500

    Adding the first part of the project

--- batch_rename.py
+++ batch_rename.py
@@ -4,11 +4,12 @@ import os
 search = "file"
 replace = "document"
 folder = "new_folder"
+type_filter = ".py"
 
 
 # get all files from current directory 
-dir_content = os.listdir('.')
-docs = [doc for doc in dir_content if os.path.isfile(doc)]
+dir_content = os.listdir(folder)
+docs = [os.path.join(folder,doc) for doc in dir_content if os.path.isfile(os.path.join(folder, doc))]
 renamed = 0
 
 print(f"{len(docs)} of {len(dir_content)} elements are files.")
@@ -20,7 +21,7 @@ for doc in docs:
     
     #replace with the given text 
     new_name = doc.replace(search, replace)
-    # os.rename(doc, new_name)
+    os.rename(doc, new_name)
     renamed += 1
     
     print(f"Renamed file {doc} to {new_name}")
